56de9ea9edb872de4e758cc7bcdc9a20e0bdafa5
jaychia <jaychia94@gmail.com>
2018-04-28 01:18:56 -0400

    alex change

--- graphstate.mli
+++ graphstate.mli
@@ -1,6 +1,8 @@
+open Owl
+
 (* Graph state object *) 
 type st
 type nodeid = string 
 val empty : st
-val add_placeholder: nodeid -> string -> st
-val add_variable : nodeid -> string -> st
+val add_placeholder: nodeid -> Arr.arr -> st
+val add_variable : nodeid -> Arr.arr -> st
