6934f784cf6ac8a923e5215dc1b3a2f12ce5a233
Alex Yoo <ay244@cornell.edu>
2018-04-28 01:50:24 -0400

    graphstate.mli init

--- graphstate.mli
+++ graphstate.mli
@@ -2,7 +2,20 @@ open Owl
 
 (* Graph state object *) 
 type st
+
 type nodeid = string 
+
+(* [empty] initializes an empty mapping *)
 val empty : st
-val add_placeholder: nodeid -> Arr.arr -> st
-val add_variable : nodeid -> Arr.arr -> st
+
+(* [add_node] adds a node to the current mapping 
+ * [requires] : node name, corresponding matrix, current graphstate
+ * [returns] : new graphstate with the node added
+ *)
+val add_node : nodeid -> Arr.arr -> st -> st
+
+(* [get_node] gets a node from the current mapping 
+ * [requires] : node name and the current graphstate
+ * [returns] : corresponding matrix
+ *)
+val get_node : nodeid -> st -> Arr.arr
