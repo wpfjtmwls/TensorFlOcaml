3d0b2f87a23b8aef5d54681ca0c243b4865fc9f5
jaychia <jaychia94@gmail.com>
2018-04-28 04:14:06 -0400

    mike found the bug

--- test.ml
+++ test.ml
@@ -10,16 +10,17 @@ let (a, graph) = graph |> Grapho.Graph.variable [1;1]
 let (x, graph) = graph |> Grapho.Graph.placeholder [1;1]
 let (y, graph) = graph |> Grapho.Graph.matmul a x
 let graphstate = GraphState.(empty
-                   |> add_node x.id (Arr.ones [|5;1|])
-                   |> add_node a.id (Arr.ones [|5;5|]))           
+                   |> add_node x.id (Arr.ones [|2;1|])
+                   |> add_node a.id (Arr.(mul_scalar (ones [|2;2|]) 5.)))
 let easy_matmul, st = Grapho.Graph.forward y graph graphstate
  
 let tests = [
-  ("easy matmul", (easy_matmul, "A=[1],x=[1], Ax=1"), "1. 1. 1. 1. 1. 1. 1. 1. 1. 1. 1. 1. 1. 1. 1. 1. 1. 1. 1. 1. 1. 1. 1. 1. 1.")
+  ("easy matmul", (easy_matmul, "A=[1],x=[1], Ax=1"), " 5. 5. 5. 5.")
 ]
 let to_string ar =
   let array = Arr.to_array ar in
   Array.fold_left (fun acc i -> acc ^ " " ^ (string_of_float i)) "" array
+
 let make_tests t (result, in_str) out_str =
   ("\n########################### " ^ t ^ " ##################################\n\n    "
   ^ in_str ^ " ========= EVALUATED TO ======> " ^ (to_string result) ^ "   \n\n"
