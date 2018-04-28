b700a124f675dcc536f90fe4d36d33dce9bb6f29
jaychia <jaychia94@gmail.com>
2018-04-28 04:08:03 -0400

    unscrewed

--- test.ml
+++ test.ml
@@ -1,13 +1,10 @@
-(* open OUnit2
+open OUnit2
 open Graphst
 open Grapho
 open Owl
-
-
 (* OCaml allows {|...|} as a syntax for strings in which the ...
    can contain unescaped quotes.  This is super useful for
    constructing test cases, as shown below. *)
-
 let graph = Grapho.Graph.empty
 let (a, graph) = graph |> Grapho.Graph.variable [1;1]
 let (x, graph) = graph |> Grapho.Graph.placeholder [1;1]
@@ -20,17 +17,14 @@ let easy_matmul, st = Grapho.Graph.forward y graph graphstate
 let tests = [
   ("easy matmul", (easy_matmul, "A=[1],x=[1], Ax=1"), "1")
 ]
-
 let to_string ar =
   let array = Arr.to_array ar in
   Array.fold_left (fun acc i -> acc ^ " " ^ (string_of_float i)) "" array
-
 let make_tests t (result, in_str) out_str =
   ("\n########################### " ^ t ^ " ##################################\n\n    "
   ^ in_str ^ " ========= EVALUATED TO ======> " ^ (to_string result) ^ "   \n\n"
   ^ "   EXPECTED OUTPUT = " ^ out_str ^ "   \n\n"
   ^ "#####################################################################\n"
   >:: (fun _ -> assert_equal out_str (to_string result)))
-
 let _ = run_test_tt_main ("suite" >::: 
-  List.map (fun (t, i, o) -> make_tests t i o) tests) *)
+  List.map (fun (t, i, o) -> make_tests t i o) tests)
