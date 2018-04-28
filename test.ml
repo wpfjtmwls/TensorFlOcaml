50e90a330f0ffe25779eda7b9dc27c3b60c250ca
evan <eba33@cornell.edu>
2018-04-28 03:50:57 -0400

    testing

--- test.ml
+++ test.ml
@@ -15,18 +15,22 @@ let (y, graph) = graph |> Grapho.Graph.matmul a x
 let graphstate = GraphState.(empty
                    |> add_node x.id (Arr.ones [|1|])
                    |> add_node a.id (Arr.ones [|1|]))           
-let easy_matmul = Grapho.Graph.forward y graph graphstate
+let easy_matmul, st = Grapho.Graph.forward y graph graphstate
  
 let tests = [
   ("easy matmul", (easy_matmul, "A=[1],x=[1], Ax=1"), "1")
 ]
 
+let to_string ar =
+  let array = Arr.to_array ar in
+  Array.fold_left (fun acc i -> acc ^ " " ^ (string_of_float i)) "" array
+
 let make_tests t (result, in_str) out_str =
   ("\n########################### " ^ t ^ " ##################################\n\n    "
-  ^ in_str ^ " ========= EVALUATED TO ======> " ^ ("result") ^ "   \n\n"
+  ^ in_str ^ " ========= EVALUATED TO ======> " ^ (to_string result) ^ "   \n\n"
   ^ "   EXPECTED OUTPUT = " ^ out_str ^ "   \n\n"
   ^ "#####################################################################\n"
-  >:: (fun _ -> assert_equal out_str ("result")))
+  >:: (fun _ -> assert_equal out_str (to_string result)))
 
 let _ = run_test_tt_main ("suite" >::: 
   List.map (fun (t, i, o) -> make_tests t i o) tests)
