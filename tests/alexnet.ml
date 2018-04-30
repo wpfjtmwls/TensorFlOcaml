0e2e047fc47934a35af7b2a37fdad7cf08283395
Alex Yoo <ay244@cornell.edu>
2018-04-30 03:02:19 -0400

    fixing alexnet

--- tests/alexnet.ml
+++ tests/alexnet.ml
@@ -4,10 +4,19 @@ open Tfgraph
 open Tfgraphst
 open Tfnode
 
-module type Chain = 
-  functor (SB_1 : SUBGRAPH) (SB_2 : SUBGRAPH) -> (*SUBGRAPH *)
-  sig
-    val create : node list -> (node * Graph.t * GraphState.st)
-  end
+module AlexNet : SUBGRAPH = struct
+  let default_name = "ALEXNET"
 
+  let create in_list name graph graphst =
+      if List.length in_list <> 1 then failwith "Wrong number of arguments" else
+      let input = List.nth in_list 0 in
+      let num_dims = (List.nth input.size 0) in
+      let (a1, graph) = graph |> Graph.variable ~prefix:name [num_dims;1] in
+      let (h1, graph) = graph |> Graph.matmul ~prefix:name input a1 in
+
+      let graphstate = GraphState.(graphst
+                      |> add_node a1 (Arr.mul_scalar (Arr.ones [|num_dims;1|]) 2)
+      ) in
+      (h1, graph, graphstate)
+end
 
