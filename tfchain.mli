dc7b4541edca1c2edb613551fba517559aaf766c
Alex Yoo <ay244@cornell.edu>
2018-04-30 00:56:07 -0400

    tfchain mli file changes according to subgraph spec

--- tfchain.mli
+++ tfchain.mli
@@ -1,6 +1,8 @@
-open Tfsubgraph
+open Tfnode
+open Tfgraph
+open Tfgraphst
 
 module type Chain = 
   functor (SB_1 : SUBGRAPH) (SB_2 : SUBGRAPH) -> sig
-    val chaingraphs : node list -> node
+    val chaingraphs : node list -> (node * Graph.t * GraphState.st)
   end
