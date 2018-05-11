d3a54e28eebe55f7ebbf17b9ff17da75299024c2
jaychia <jaychia94@gmail.com>
2018-05-11 13:48:59 -0400

    clearer

--- test_mnist.ml
+++ test_mnist.ml
@@ -8,26 +8,31 @@ open Mnistnet
 
 
 (* Pull data *)
-let batchsize = 32
 let xtrain, _, ytrain = Dataset.load_mnist_train_data ()
 let xtrain, ytrain = (Dense.Matrix.Generic.cast_s2d xtrain, Dense.Matrix.Generic.cast_s2d ytrain)
+let xtest, _, ytest = Dataset.load_mnist_test_data ()
+let xtest, ytest = (Dense.Matrix.Generic.cast_s2d xtest, Dense.Matrix.Generic.cast_s2d ytest)
+
+(* Normalize data *)
 let mean = Arr.mean ~axis:0 xtrain
 let std= Arr.std ~axis:0 xtrain
 let std = Arr.map (fun x -> if x = 0. then 1. else x) std
 let xtrain = Arr.div (Arr.sub xtrain mean) std
-let xtrain = Arr.concat_horizontal xtrain (Arr.ones [|(Arr.shape xtrain).(0);1|]) (* Give features extra dimension for bias *)
+let xtest = Arr.div (Arr.sub xtest mean) std
+
+(* Extra dimensions for bias *)
+let xtrain = Arr.concat_horizontal xtrain (Arr.ones [|(Arr.shape xtrain).(0);1|])
+let xtest = Arr.concat_horizontal xtest (Arr.ones [|(Arr.shape xtest).(0);1|])
+
+(* Split data in batches of size batchsize *)
+let batchsize = 32
 let xtrainbatches = Arr.split ~axis:0 (Array.of_list (List.init (60000 / batchsize) (fun _ -> batchsize))) xtrain
 let ytrainbatches = Arr.split ~axis:0 (Array.of_list (List.init (60000 / batchsize) (fun _ -> batchsize))) ytrain
 let trainbatches = List.combine (Array.to_list xtrainbatches) (Array.to_list ytrainbatches)
-let trainbatches = [List.nth trainbatches 0]
-let xtest, _, ytest = Dataset.load_mnist_test_data ()
-let xtest, ytest = (Dense.Matrix.Generic.cast_s2d xtest, Dense.Matrix.Generic.cast_s2d ytest)
-let xtest = Arr.div (Arr.sub xtest mean) std
-let xtest = Arr.concat_horizontal xtest (Arr.ones [|(Arr.shape xtest).(0);1|]) (* Give features extra dimension for bias *)
 let xtest, ytest = ((Arr.get_slice [[0;(batchsize - 1)]] xtest), (Arr.get_slice [[0;(batchsize - 1)]] ytest))
 
-(* let _ = Printf.printf "%s\n" (Array.fold_left (fun acc i -> acc ^ "x" ^ (string_of_int i)) "" (Arr.shape xtrainbatches.(0)))
-let _ = Printf.printf "%s\n" (Array.fold_left (fun acc i -> acc ^ "x" ^ (string_of_int i)) "" (Arr.shape xtest)) *)
+(* TODO: Remove. For now, try overfitting on just the first batch of training data *)
+let trainbatches = [List.nth trainbatches 0]
 
 (* Graph construction *)
 let graph = Graph.empty
